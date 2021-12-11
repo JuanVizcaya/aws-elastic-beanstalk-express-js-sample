9504f4fcf7fa4018539a2ea7f7d2c810ccbe4c8f
JuanVizcaya <juanj.vizcaya@gmail.com>
2021-12-11 01:33:25 -0600

    added first name to greetings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Vizcaya!'));
+app.get('/', (req, res) => res.send('Hello Juan Vizcaya!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
